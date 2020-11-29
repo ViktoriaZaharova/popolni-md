801a0803c92ade46540b563270c5590161836a15
viktoriazaharova <zaharova181114@gmail.com>
2020-11-29 16:51:54 +0200

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -39,3 +39,26 @@ $('ul.tabs__caption').on('click', 'li:not(.active)', function () {
 });
 
 new WOW().init();
+
+$( ".button_su_inner" ).mouseenter(function(e) {
+    var parentOffset = $(this).offset();
+
+    var relX = e.pageX - parentOffset.left;
+    var relY = e.pageY - parentOffset.top;
+    $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
+    $(this).prev(".su_button_circle").removeClass("desplode-circle");
+    $(this).prev(".su_button_circle").addClass("explode-circle");
+
+});
+
+$( ".button_su_inner" ).mouseleave(function(e) {
+
+    var parentOffset = $(this).offset();
+
+    var relX = e.pageX - parentOffset.left;
+    var relY = e.pageY - parentOffset.top;
+    $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
+    $(this).prev(".su_button_circle").removeClass("explode-circle");
+    $(this).prev(".su_button_circle").addClass("desplode-circle");
+
+});
